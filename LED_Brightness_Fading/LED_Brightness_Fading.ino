e8cfb7c3eca7ebcd10ef3374d559c8a5b5a56fbd
XD-DENG <xd_deng@hotmail.com>
2016-04-22 23:47:37 +0800

    example "LED brightness fading"

---
+++ LED_Brightness_Fading/LED_Brightness_Fading.ino
@@ -0,0 +1,28 @@
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 3;    // how many points to fade the LED by
+
+
+
+void setup()  { 
+  // declare pin 14 to be an output:
+  pinMode(GREEN_LED, OUTPUT);
+
+} 
+
+void loop()  { 
+  // set the brightness of pin 9:
+  analogWrite(GREEN_LED, brightness);
+
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount; 
+  }     
+
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
+}
